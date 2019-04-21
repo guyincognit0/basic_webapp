aa262fa5fbf4a15ac5edea5542ffb85a22672a88
guyincognit0 <no-reply@.com>
2019-04-21 17:04:01 +1000

    Use local assets and fix job loader

--- frontend/src/components/LoadingPlaceholder.js
+++ frontend/src/components/LoadingPlaceholder.js
@@ -3,14 +3,13 @@ import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
 
 
 function LoadingPlaceholder(props) {
-  // TODO: use local assets
   return (
     <div>
-      <Segment>
+      <Segment placeholder>
         <Dimmer active inverted>
           <Loader inverted content='Loading' />
         </Dimmer>
-        <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+        <Image src='static/short-paragraph.png' />
       </Segment>
     </div>
   )
